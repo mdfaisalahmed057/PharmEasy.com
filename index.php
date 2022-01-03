cc2b8f17415ec1f332a5b4cd3ad504d6adabfea6
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-01-03 13:31:15 +0530

    edite the main page

--- index.php
+++ index.php
@@ -10,39 +10,39 @@ include "includes/head.php"
   session_start();
   include "includes/header.php";
   ?>
-  <div class="container-fluid ">
 
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="images/carousel1.jpg" class="d-block w-100 `" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="images/carousel2.jpg" class="d-block w-100 `" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="images/carousel3.jpg" class="d-block w-100 `" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src=" images/carousel4.jpg" class="d-block w-100 `" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src=" images/carousel5.jpg" class="d-block w-100 ` " alt="...">
-        </div>
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/carousel1.jpg" class="d-block w-100 `" alt="...">
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+      <div class="carousel-item">
+        <img src="images/carousel2.jpg" class="d-block w-100 `" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/carousel3.jpg" class="d-block w-100 `" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src=" images/carousel4.jpg" class="d-block w-100 `" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src=" images/carousel5.jpg" class="d-block w-100 ` " alt="...">
+      </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
 
 
-    <!----------------       end of carousel                     --------->
+  <!----------------       end of carousel                     --------->
+  <div class="container-fluid ">
 
     <!-- categories-->
 

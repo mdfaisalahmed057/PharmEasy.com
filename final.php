2a0fe7e01bdf2918765145b906153763c079dd73
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-03-18 17:20:47 +0530

    change the final page

--- final.php
+++ final.php
@@ -6,7 +6,13 @@ include "includes/head.php";
     <?php
     include "includes/header.php";
     ?>
-    <img src="images/final.jpg" alt="" style="width:30rem; margin-left: 400px;margin-top: 10px ">
+    <div>
+        <h1 style="margin-left: 380px;">WE'VE RECEIVED YOUR ORDER!</h1>
+        <h5 style="margin-left: 250px;">Thanks for shopping with PharmEasy!
+            you can check you order details in orders section</h5>
+        <img src="images/final.gif" alt="" style="width:30rem; margin-left: 400px;">
+    </div>
+    <a href="index.php"> <button style="margin-left: 530px; margin-top: -10px;" type="button" class="btn btn-outline-info btn-lg">Go back to the store </button></a>
     <?php
     add_order();
     ?>
